aefba5599fefb48324000460593fe4d49774d158
Riju Mukherjee <64312216+rijulizer@users.noreply.github.com>
2025-03-27 16:59:28 +0100

    Add device support and improve comments

--- agents.py
+++ agents.py
@@ -2,25 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributions as distributions
-from einops import rearrange, repeat
-from einops.layers.torch import Rearrange
 import copy
 from torch.cuda.amp import autocast
 
 from sub_models.functions_losses import SymLogTwoHotLoss
 from utils import EMAScalar
-
-device = torch.device(
-    "mps"
-    if torch.backends.mps.is_available()
-    else ("cuda" if torch.cuda.is_available() else "cpu")
-)
+from sub_models.constants import DEVICE
 
 
 def percentile(x, percentage):
     flat_x = torch.flatten(x)
     kth = int(percentage * len(flat_x))
-    per = torch.kthvalue(flat_x, kth).values
+    if DEVICE.type == "mps":
+        # MPS does not support kthvalue, use sorting instead
+        sorted_x, _ = torch.sort(flat_x)
+        per = sorted_x[kth]
+    else:
+        per = torch.kthvalue(flat_x, kth + 1).values
     return per
 
 
@@ -31,7 +29,7 @@ def calc_lambda_return(rewards, values, termination, gamma, lam, dtype=torch.flo
     batch_size, batch_length = rewards.shape[:2]
     # gae_step = torch.zeros((batch_size, ), dtype=dtype, device=device)
     gamma_return = torch.zeros(
-        (batch_size, batch_length + 1), dtype=dtype, device=device
+        (batch_size, batch_length + 1), dtype=dtype, device=DEVICE.type
     )
     gamma_return[:, -1] = values[:, -1]
     for t in reversed(range(batch_length)):  # with last bootstrap
@@ -53,91 +51,112 @@ class ActorCriticAgent(nn.Module):
         self.entropy_coef = entropy_coef
         self.use_amp = True
         self.tensor_dtype = torch.float16 if self.use_amp else torch.float32
-
         self.symlog_twohot_loss = SymLogTwoHotLoss(255, -20, 20)
 
-        actor = [
+        # Sequential actor model to map from feat_dim to action_dim
+        actor_model = [
             nn.Linear(feat_dim, hidden_dim, bias=False),
             nn.LayerNorm(hidden_dim),
             nn.ReLU(),
         ]
         for i in range(num_layers - 1):
-            actor.extend(
+            actor_model.extend(
                 [
                     nn.Linear(hidden_dim, hidden_dim, bias=False),
                     nn.LayerNorm(hidden_dim),
                     nn.ReLU(),
                 ]
             )
-        self.actor = nn.Sequential(*actor, nn.Linear(hidden_dim, action_dim))
+        self.actor = nn.Sequential(*actor_model, nn.Linear(hidden_dim, action_dim))
 
-        critic = [
+        # Sequential critic model to map from feat_dim to 255 dim
+        critic_model = [
             nn.Linear(feat_dim, hidden_dim, bias=False),
             nn.LayerNorm(hidden_dim),
             nn.ReLU(),
         ]
         for i in range(num_layers - 1):
-            critic.extend(
+            critic_model.extend(
                 [
                     nn.Linear(hidden_dim, hidden_dim, bias=False),
                     nn.LayerNorm(hidden_dim),
                     nn.ReLU(),
                 ]
             )
-
-        self.critic = nn.Sequential(*critic, nn.Linear(hidden_dim, 255))
+        self.critic = nn.Sequential(*critic_model, nn.Linear(hidden_dim, 255))
+        # Make a copy of critic for slow critic
         self.slow_critic = copy.deepcopy(self.critic)
 
         self.lowerbound_ema = EMAScalar(decay=0.99)
         self.upperbound_ema = EMAScalar(decay=0.99)
 
         self.optimizer = torch.optim.Adam(self.parameters(), lr=3e-5, eps=1e-5)
-        # enable scaler based on device type
+        # Enable scaler based on DEVICE type
         self.scaler = (
             torch.cuda.amp.GradScaler(enabled=self.use_amp)
-            if device.type == "cuda"
+            if DEVICE.type == "cuda"
             else None
         )
 
     @torch.no_grad()
     def update_slow_critic(self, decay=0.98):
+        """
+        Update slow critic models parameters with decay.
+        """
         for slow_param, param in zip(
             self.slow_critic.parameters(), self.critic.parameters()
         ):
             slow_param.data.copy_(slow_param.data * decay + param.data * (1 - decay))
 
     def policy(self, x):
+        """
+        Use the logits of the actor model to get the policy distribution.
+        """
         logits = self.actor(x)
         return logits
 
     def value(self, x):
+        """
+        Use the critic model to get the value of the state.
+        """
         value = self.critic(x)
         value = self.symlog_twohot_loss.decode(value)
         return value
 
     @torch.no_grad()
     def slow_value(self, x):
+        """
+        Use the slow critic model to get the slow-value of the state.
+        """
         value = self.slow_critic(x)
         value = self.symlog_twohot_loss.decode(value)
         return value
 
     def get_logits_raw_value(self, x):
+        """
+        Get the raw actor logits and raw critiic value from the actor and critic models.
+        """
         logits = self.actor(x)
         raw_value = self.critic(x)
         return logits, raw_value
 
     @torch.no_grad()
     def sample(self, latent, greedy=False):
+        """
+        Get the action using the policy distribution (Actor model) from the latent state.
+        Based on greedy or sampling.
+        """
         self.eval()
         with torch.autocast(
-            device_type=device.type, dtype=torch.float16, enabled=self.use_amp
+            device_type=DEVICE.type, dtype=torch.float16, enabled=self.use_amp
         ):
-            logits = self.policy(latent)
-            dist = distributions.Categorical(logits=logits)
+
+            action_logits = self.policy(latent)
+            action_dist = distributions.Categorical(logits=action_logits)
             if greedy:
-                action = dist.probs.argmax(dim=-1)
+                action = action_dist.probs.argmax(dim=-1)
             else:
-                action = dist.sample()
+                action = action_dist.sample()
         return action
 
     def sample_as_env_action(self, latent, greedy=False):
@@ -152,7 +171,7 @@ class ActorCriticAgent(nn.Module):
         """
         self.train()
         with torch.autocast(
-            device_type=device.type, dtype=torch.float16, enabled=self.use_amp
+            device_type=DEVICE.type, dtype=torch.float16, enabled=self.use_amp
         ):
             logits, raw_value = self.get_logits_raw_value(latent)
             dist = distributions.Categorical(logits=logits[:, :-1])
@@ -181,7 +200,7 @@ class ActorCriticAgent(nn.Module):
             upper_bound = self.upperbound_ema(percentile(lambda_return, 0.95))
             S = upper_bound - lower_bound
             norm_ratio = torch.max(
-                torch.ones(1).to(device), S
+                torch.ones(1).to(DEVICE), S
             )  # max(1, S) in the paper
             norm_advantage = (lambda_return - value[:, :-1]) / norm_ratio
             policy_loss = -(log_prob * norm_advantage.detach()).mean()
